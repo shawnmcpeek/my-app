b20744c199dc2d68d372f55238e077f9bf24d9fe
Shawn McPeek <shawnmcpeek@gmail.com>
2023-11-05 19:46:18 -0500

    modified:   src/containers/App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,25 +28,22 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
       // Filter robots based on the 'searchfield'
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f2">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
