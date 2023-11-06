bc83ca6af9dcd0e8347b8a649dd0092a89d14361
Shawn McPeek <shawnmcpeek@gmail.com>
2023-11-05 19:23:10 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/App.js
    	deleted:    src/App.test.js
    	new file:   src/Card.js
    	new file:   src/CardList.js
    	new file:   src/SEGA.woff
    	new file:   src/Scroll.js
    	new file:   src/SearchBox.js
    	modified:   src/index.css
    	modified:   src/index.js
    	deleted:    src/logo.svg
    	new file:   src/registerServiceWorker.js
    	new file:   src/robots.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+import Scroll from "./Scroll";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { robots: [], searchfield: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  onSearchChange = (event) => {
+    // Update the 'searchfield' state based on user input
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      // Filter robots based on the 'searchfield'
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+import { robots } from "./robots";
+
+
+const CardList = ({ robots }) => {
+  const CardComponent = robots.map((robot, i) => (
+    <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
+  ));
+  return <div>{CardComponent}</div>;
+};
+
+export default CardList;
